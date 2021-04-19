e20f082541e4e064d627d548c3de434a85f9bc34
wangzheqie <wangzheqie@qq.com>
2021-04-19 20:25:30 +0800

    add ranger vehicle

---
+++ apps/ranger_demo/ranger_demo.cpp
@@ -0,0 +1,57 @@
+/**
+ * @Kit       : Qt-Creator: Desktop
+ * @Author    : Wang Zhe
+ * @Date      : 2021-04-19  19:41:52
+ * @FileName  : ranger_demo.cpp
+ * @Mail      : wangzheqie@qq.com
+ * Copyright  : AgileX Robotics
+ **/
+
+#include "ugv_sdk/ranger_base.hpp"
+
+using namespace westonrobot;
+
+int main(int argc, char *argv[]) {
+  std::string device_name;
+
+  if (argc == 2) {
+    device_name = {argv[1]};
+    std::cout << "Specified CAN: " << device_name << std::endl;
+  } else {
+    std::cout << "Usage: app_ranger_demo <interface>" << std::endl
+              << "Example 1: ./app_ranger_demo can0" << std::endl;
+    return -1;
+  }
+
+  RangerBase ranger;
+  ranger.Connect(device_name);
+
+  ranger.EnableCommandedMode();
+
+  int count = 0;
+  while (true) {
+    if (count < 100) {
+      std::cout << "Motor: 0.2, 0" << std::endl;
+      ranger.SetMotionCommand(0.2, 0.0);
+    }
+
+    auto state = ranger.GetRangerState();
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "count: " << count << std::endl;
+    std::cout << "control mode: "
+              << static_cast<int>(state.system_state.control_mode)
+              << " , vehicle state: "
+              << static_cast<int>(state.system_state.vehicle_state)
+              << std::endl;
+    std::cout << "battery voltage: " << state.system_state.battery_voltage
+              << std::endl;
+    std::cout << "velocity (linear, angular): "
+              << state.motion_state.linear_velocity << ", "
+              << state.motion_state.angular_velocity << std::endl;
+    std::cout << "-------------------------------" << std::endl;
+
+    usleep(20000);
+    ++count;
+  }
+  return 0;
+}
---
+++ include/ugv_sdk/ranger_base.hpp
@@ -0,0 +1,82 @@
+/**
+ * @Kit       : Qt-Creator: Desktop
+ * @Author    : Wang Zhe
+ * @Date      : 2021-04-19  19:12:52
+ * @FileName  : ranger_base.hpp
+ * @Mail      : wangzheqie@qq.com
+ * Copyright  : AgileX Robotics
+ **/
+
+#ifndef RANGER_BASE_HPP
+#define RANGER_BASE_HPP
+
+#include <cstdint>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "ugv_sdk/agilex_base.hpp"
+
+namespace westonrobot {
+struct RangerState {
+  // system state
+  SystemStateMessage system_state;
+  MotionStateMessage motion_state;
+  LightStateMessage light_state;
+
+  RcStateMessage rc_state;
+
+  ActuatorHSStateMessage actuator_hs_state[4];
+  ActuatorLSStateMessage actuator_ls_state[4];
+
+  // sensor data
+  OdometryMessage odometry;
+};
+
+struct RangerMotionCmd {
+  double linear_velocity;
+  double angular_velocity;
+};
+
+struct RangerLightCmd {
+  RangerLightCmd() = default;
+  RangerLightCmd(LightMode f_mode, uint8_t f_value, LightMode r_mode,
+                 uint8_t r_value)
+      : cmd_ctrl_allowed(true),
+        front_mode(f_mode),
+        front_custom_value(f_value),
+        rear_mode(r_mode),
+        rear_custom_value(r_value) {}
+
+  bool cmd_ctrl_allowed = false;
+  LightMode front_mode;
+  uint8_t front_custom_value;
+  LightMode rear_mode;
+  uint8_t rear_custom_value;
+};
+
+/////////////////////////////////////////////////////////////////////////
+
+class RangerBase : public AgilexBase {
+ public:
+  RangerBase() : AgilexBase(){};
+  ~RangerBase() = default;
+
+  // set up connection
+  void Connect(std::string dev_name) override;
+
+  // robot control
+  void SetMotionCommand(double linear_vel, double angular_vel);
+  void SetLightCommand(const RangerLightCmd &cmd);
+
+  // get robot state
+  RangerState GetRangerState();
+
+ private:
+  RangerState ranger_state_;
+
+  void ParseCANFrame(can_frame *rx_frame) override;
+  void UpdateRangerState(const AgxMessage &status_msg, RangerState &state);
+};
+}  // namespace westonrobot
+#endif  // RANGER_BASE_HPP
