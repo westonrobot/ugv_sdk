987b2a057d76caaadba49af5babd0d1bef87e059
wangzheqie <wangzheqie@qq.com>
2021-06-29 17:06:03 +0800

    add limon base class

---
+++ include/ugv_sdk/limon_base.h
@@ -0,0 +1,78 @@
+#ifndef LIMON_BASE_H
+#define LIMON_BASE_H
+
+#include <cstdint>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "ugv_sdk/agilex_base_serialport.h"
+
+namespace westonrobot {
+struct LimonState {
+  // system state
+  SystemStateMessage system_state;
+  MotionStateMessage motion_state;
+  LightStateMessage light_state;
+
+
+
+  RcStateMessage rc_state;
+
+  ActuatorHSStateMessage actuator_hs_state[8];
+  ActuatorLSStateMessage actuator_ls_state[8];
+  MotionModeFeedbackMessage current_motion_mode;
+
+  // sensor data
+  OdometryMessage odometry;
+};
+
+struct LimonMotionCmd {
+  double linear_velocity;
+  double angular_velocity;
+};
+
+struct LimonLightCmd {
+  LimonLightCmd() = default;
+  LimonLightCmd(LightMode f_mode, uint8_t f_value, LightMode r_mode,
+                 uint8_t r_value)
+      : cmd_ctrl_allowed(true),
+        front_mode(f_mode),
+        front_custom_value(f_value),
+        rear_mode(r_mode),
+        rear_custom_value(r_value) {}
+
+  bool cmd_ctrl_allowed = false;
+  LightMode front_mode;
+  uint8_t front_custom_value;
+  LightMode rear_mode;
+  uint8_t rear_custom_value;
+};
+
+/////////////////////////////////////////////////////////////////////////
+
+class LimonBase : public AgilexBase {
+ public:
+  LimonBase() : AgilexBase(){};
+  ~LimonBase() = default;
+
+  // set up connection
+  void Connect(std::string dev_name) override;
+
+  // robot control
+  void SetMotionCommand(double linear_vel, double steer_angle,
+                        double lateral_vel = 0.0, double angular_vel = 0.0);
+  void SetLightCommand(const LimonLightCmd &cmd);
+  void SetMotionMode(uint8_t mode);
+
+  // get robot state
+  LimonState GetLimonState();
+
+ private:
+  LimonState ranger_state_;
+
+  void ParseCANFrame(can_frame *rx_frame) override;
+  void UpdateLimonState(const AgxMessage &status_msg, LimonState &state);
+};
+}  // namespace westonrobot
+#endif  // LIMON_BASE_H
---
+++ src/limon_base.cpp
@@ -0,0 +1,102 @@
+#include "ugv_sdk/limon_base.h"
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <ratio>
+#include <string>
+#include <thread>
+
+#include "stopwatch.hpp"
+
+#include "ugv_sdk/details/agilex_msg_parser.h"
+
+namespace westonrobot {
+void LimonBase::Connect(std::string dev_name) {
+  AgilexBase::Connect(dev_name, std::bind(&LimonBase::ParseCANFrame, this,
+                                          std::placeholders::_1));
+}
+
+void LimonBase::SetMotionCommand(double linear_vel, double steer_angle,
+                                  double lateral_vel, double angular_vel) {
+  AgilexBase::SetMotionCommand(linear_vel, angular_vel, lateral_vel,
+                               steer_angle / 10.0);
+}
+
+void LimonBase::SetLightCommand(const LimonLightCmd &cmd) {
+  if (cmd.cmd_ctrl_allowed) {
+    AgilexBase::SendLightCommand(cmd.front_mode, cmd.front_custom_value,
+                                 LightMode::CONST_OFF, 0);
+  }
+}
+
+void LimonBase::SetMotionMode(uint8_t mode) {
+  AgilexBase::SetMotionMode(mode);
+}
+
+LimonState LimonBase::GetLimonState() {
+  std::lock_guard<std::mutex> guard(state_mutex_);
+  return ranger_state_;
+}
+
+void LimonBase::ParseCANFrame(can_frame *rx_frame) {
+  AgxMessage status_msg;
+  DecodeCanFrame(rx_frame, &status_msg);
+  std::lock_guard<std::mutex> guard(state_mutex_);
+  UpdateLimonState(status_msg, ranger_state_);
+}
+
+void LimonBase::UpdateLimonState(const AgxMessage &status_msg,
+                                   LimonState &state) {
+  switch (status_msg.type) {
+    case AgxMsgSystemState: {
+      //   std::cout << "system status feedback received" << std::endl;
+      state.system_state = status_msg.body.system_state_msg;
+      break;
+    }
+    case AgxMsgMotionState: {
+      //  std::cout << "motion control feedback received" << std::endl;
+      state.motion_state = status_msg.body.motion_state_msg;
+      state.motion_state.steering_angle *= 10;
+      // std::cout << "steering angle: " << state.motion_state.steering_angle <<
+      // std::endl;
+      break;
+    }
+    case AgxMsgLightState: {
+      // std::cout << "light control feedback received" << std::endl;
+      state.light_state = status_msg.body.light_state_msg;
+      break;
+    }
+    case AgxMsgRcState: {
+      state.rc_state = status_msg.body.rc_state_msg;
+      break;
+    }
+    case AgxMsgActuatorHSState: {
+      // std::cout << "actuator hs feedback received" << std::endl;
+      state.actuator_hs_state[status_msg.body.actuator_hs_state_msg.motor_id] =
+          status_msg.body.actuator_hs_state_msg;
+      break;
+    }
+    case AgxMsgActuatorLSState: {
+      // std::cout << "actuator ls feedback received" << std::endl;
+      state.actuator_ls_state[status_msg.body.actuator_ls_state_msg.motor_id] =
+          status_msg.body.actuator_ls_state_msg;
+      break;
+    }
+    case AgxMsgMotionModeState: {
+      state.current_motion_mode = status_msg.body.motion_mode_feedback_msg;
+      break;
+    }
+    /* sensor feedback */
+    case AgxMsgOdometry: {
+      // std::cout << "Odometer msg feedback received" << std::endl;
+      state.odometry = status_msg.body.odometry_msg;
+    }
+    default:
+      break;
+  }
+}
+}  // namespace westonrobot
