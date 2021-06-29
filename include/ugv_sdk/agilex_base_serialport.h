c00f3218412f6649cad3599acc0b6ededa66dea3
wangzheqie <wangzheqie@qq.com>
2021-06-29 16:43:03 +0800

    add seiral port base class

---
+++ include/ugv_sdk/agilex_base_serialport.h
@@ -0,0 +1,101 @@
+
+#ifndef AGILEX_BASE_SERIALPORT_HPP
+#define AGILEX_BASE_SERIALPORT_HPP
+
+#include <atomic>
+#include <cstdint>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "async_port/async_serial.hpp"
+#include "ugv_sdk/agilex_message.h"
+#include "ugv_sdk/details/agilex_msg_parser.h"
+
+namespace westonrobot {
+
+/**
+ * the base class that use serial port
+ */
+class AgilexBaseSerialPort {
+ public:
+  AgilexBaseSerialPort() = default;
+  virtual ~AgilexBaseSerialPort();
+
+  // do not allow copy or assignment
+  AgilexBaseSerialPort(const AgilexBaseSerialPort &) = delete;
+  AgilexBaseSerialPort &operator=(const AgilexBaseSerialPort &) = delete;
+
+  // any derived robot must implement this method with proper call back defined
+  // usually: ttyUSB0, ttyTHS1
+  virtual void Connect(std::string dev_name) = 0;
+
+  // cmd thread runs at 50Hz (20ms) by default
+  void SetCmdThreadPeriodMs(int32_t period_ms) {
+    cmd_thread_period_ms_ = period_ms;
+  };
+
+  // timeout: robot stops if user does not call SetMotionCommand() periodically
+  void EnableCmdTimeout(uint32_t timeout_ms = 100);
+  void DisableTimeout() { enable_timeout_ = false; }
+
+  // switch to commanded mode
+  void EnableCommandedMode();
+
+  // enforce 50Hz command loop for all AgileX robots internally
+  void SetMotionCommand(double linear_vel, double angular_vel,
+                        double lateral_velocity, double steering_angle);
+
+  // one-shot light command
+  void SendLightCommand(LightMode front_mode, uint8_t front_custom_value,
+                        LightMode rear_mode, uint8_t rear_custom_value);
+  void DisableLightControl();
+
+  // motion mode change
+  void SetMotionMode(uint8_t mode);
+
+  // reset fault states
+  void ResetRobotState();
+
+  // send the can format data
+  void SendCanFrame(const can_frame &frame);
+
+ protected:
+  std::mutex state_mutex_;
+  std::mutex motion_cmd_mutex_;
+  MotionCommandMessage current_motion_cmd_;
+
+  // communication interface
+  bool serial_connected_ = false;
+  std::shared_ptr<AsyncSerial> serial_;
+
+  // timeout to be implemented in each vehicle
+  bool enable_timeout_ = true;
+  uint32_t timeout_ms_ = 500;
+  uint32_t watchdog_counter_ = 0;
+  void FeedCmdTimeoutWatchdog() { watchdog_counter_ = 0; };
+
+  // command thread
+  std::thread cmd_thread_;
+  int32_t cmd_thread_period_ms_ = 20;
+  bool cmd_thread_started_ = false;
+  std::atomic<bool> keep_running_;
+
+  // internal functions
+  void StartCmdThread();
+  void ControlLoop(int32_t period_ms);
+
+  // connect to roboot from can or serial
+  using SerialFrameRxCallback = AsyncSerial::ReceiveCallback;
+  void Connect(std::string dev_name, SerialFrameRxCallback cb);
+  void Disconnect();
+
+  // ask background thread to shutdown properly
+  void Terminate();
+
+  void SendRobotCmd();
+  virtual void ParseSerialFrame(can_frame *rx_frame) = 0;
+};
+}  // namespace westonrobot
+
+#endif /* AGILEX_BASE_SERIALPORT_HPP */
---
+++ src/agilex_base_serialport.cpp
@@ -0,0 +1,170 @@
+#include "ugv_sdk/agilex_base_serialport.h"
+
+#include "ugv_sdk/details/agilex_msg_parser.h"
+#include "stopwatch.hpp"
+
+namespace westonrobot {
+AgilexBaseSerialPort::~AgilexBaseSerialPort() {
+  // release resource if occupied
+  Disconnect();
+
+  // joint cmd thread
+  if (cmd_thread_.joinable()) cmd_thread_.join();
+}
+
+void AgilexBaseSerialPort::Connect(std::string dev_name, SerialFrameRxCallback cb) {
+  serial_ = std::make_shared<AsyncSerial>(dev_name);
+  serial_->SetReceiveCallback(cb);
+  serial_->StartListening();
+  serial_connected_ = true;
+}
+
+void AgilexBaseSerialPort::Disconnect() {
+  if (serial_connected_) serial_->StopService();
+}
+
+void AgilexBaseSerialPort::Terminate() {
+  keep_running_ = false;
+  std::terminate();
+}
+
+void AgilexBaseSerialPort::EnableCmdTimeout(uint32_t timeout_ms) {
+  enable_timeout_ = true;
+  timeout_ms_ = timeout_ms;
+}
+
+void AgilexBaseSerialPort::StartCmdThread() {
+  keep_running_ = true;
+  cmd_thread_ = std::thread(
+      std::bind(&AgilexBaseSerialPort::ControlLoop, this, cmd_thread_period_ms_));
+  cmd_thread_started_ = true;
+}
+
+void AgilexBaseSerialPort::ControlLoop(int32_t period_ms) {
+  uint32_t timeout_iter_num;
+
+  if (enable_timeout_) {
+    if (timeout_ms_ < period_ms) timeout_ms_ = period_ms;
+    timeout_iter_num = static_cast<uint32_t>(timeout_ms_ / period_ms);
+    // std::cout << "Timeout iteration number: " << timeout_iter_num <<
+    // std::endl;
+  }
+
+  Timer tm;
+  while (keep_running_) {
+    tm.reset();
+    if (enable_timeout_) {
+      if (watchdog_counter_ < timeout_iter_num) {
+        SendRobotCmd();
+        ++watchdog_counter_;
+      }
+      //   else {
+      //     std::cout << "Warning: cmd timeout, no cmd sent to robot" <<
+      //     std::endl;
+      //   }
+    } else {
+      SendRobotCmd();
+    }
+    tm.sleep_until_ms(period_ms);
+  }
+}
+
+void AgilexBaseSerialPort::EnableCommandedMode() {
+  // construct message
+  AgxMessage msg;
+  msg.type = AgxMsgControlModeConfig;
+  msg.body.control_mode_config_msg.mode = CONTROL_MODE_UART;
+
+  // encode msg to can frame and send to bus
+  can_frame frame;
+  EncodeCanFrame(&msg, &frame);
+  SendCanFrame(frame);
+}
+
+void AgilexBaseSerialPort::SetMotionCommand(double linear_vel, double angular_vel,
+                                  double lateral_velocity,
+                                  double steering_angle) {
+  // make sure cmd thread is started before attempting to send commands
+  if (!cmd_thread_started_) StartCmdThread();
+
+  std::lock_guard<std::mutex> guard(motion_cmd_mutex_);
+  current_motion_cmd_.linear_velocity = linear_vel;
+  current_motion_cmd_.angular_velocity = angular_vel;
+  current_motion_cmd_.lateral_velocity = lateral_velocity;
+  current_motion_cmd_.steering_angle = steering_angle;
+
+  FeedCmdTimeoutWatchdog();
+}
+
+void AgilexBaseSerialPort::SendRobotCmd() {
+  if (serial_connected_) {
+    // motion control message
+    AgxMessage msg;
+    msg.type = AgxMsgMotionCommand;
+
+    motion_cmd_mutex_.lock();
+    msg.body.motion_command_msg = current_motion_cmd_;
+    motion_cmd_mutex_.unlock();
+
+    // send to can bus
+    can_frame frame;
+    EncodeCanFrame(&msg, &frame);
+    SendCanFrame(frame);
+  }
+}
+
+void AgilexBaseSerialPort::SendLightCommand(LightMode front_mode,
+                                  uint8_t front_custom_value,
+                                  LightMode rear_mode,
+                                  uint8_t rear_custom_value) {
+//   AgxMessage msg;
+//   msg.type = AgxMsgLightCommand;
+
+//   msg.body.light_command_msg.cmd_ctrl_allowed = true;
+//   msg.body.light_command_msg.front_light.mode = front_mode;
+//   msg.body.light_command_msg.front_light.custom_value = front_custom_value;
+//   msg.body.light_command_msg.rear_light.mode = rear_mode;
+//   msg.body.light_command_msg.rear_light.custom_value = rear_custom_value;
+
+//   // send to can bus
+//   can_frame frame;
+//   EncodeCanFrame(&msg, &frame);
+//   serial_->SendFrame(frame);
+}
+
+void AgilexBaseSerialPort::DisableLightControl() {
+//   AgxMessage msg;
+//   msg.type = AgxMsgLightCommand;
+
+//   msg.body.light_command_msg.cmd_ctrl_allowed = false;
+
+//   // send to can bus
+//   can_frame frame;
+//   EncodeCanFrame(&msg, &frame);
+//   serial_->SendFrame(frame);
+}
+
+void AgilexBaseSerialPort::SetMotionMode(uint8_t mode)
+{
+   AgxMessage msg;
+   msg.type = AgxMsgSetMotionMode;
+   msg.body.motion_mode_msg.motion_mode = mode;
+
+   // send to can bus
+   can_frame frame;
+   EncodeCanFrame(&msg, &frame);
+   SendCanFrame(frame);
+}
+
+void AgilexBaseSerialPort::SendCanFrame(const can_frame &frame)
+{
+  size_t len = sizeof(frame);
+  if(len <= 8){
+      return;
+  }
+  
+  uint8_t data[len]={0};
+  memcpy(&data[0], (void*)&frame, len);
+  serial_->SendBytes(data, len);
+}
+}  // namespace westonrobot
