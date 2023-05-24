1a5f9b116c41be004b47bb35e1d2f881fd37a150
Matthew Ong <matthew.ong@westonrobot.com>
2023-05-24 09:23:05 +0800

    motion state feedback - edit for ranger mini 1

--- src/mobile_robot/ranger_robot.cpp
+++ src/mobile_robot/ranger_robot.cpp
@@ -24,18 +24,29 @@ RangerCoreState RangerMiniV1Robot::GetRobotState() {
   ranger_state.time_stamp = state.time_stamp;
   ranger_state.system_state = state.system_state;
 
-  ranger_state.motion_state.linear_velocity =
-      state.motion_state.linear_velocity;
-  ranger_state.motion_state.angular_velocity =
-      -state.motion_state.angular_velocity;
-  ranger_state.motion_state.lateral_velocity =
-      state.motion_state.lateral_velocity;
-  ranger_state.motion_state.steering_angle =
-      -state.motion_state.steering_angle * 10 / 180.0 * 3.14;
-
   ranger_state.light_state = state.light_state;
   ranger_state.rc_state = state.rc_state;
   ranger_state.current_motion_mode = state.motion_mode_state;
+
+  if (ranger_state.current_motion_mode.motion_mode == 2) {
+    ranger_state.motion_state.linear_velocity = 0;
+    ranger_state.motion_state.angular_velocity =
+        -state.motion_state.linear_velocity / 0.254558;
+    ranger_state.motion_state.lateral_velocity =
+        state.motion_state.lateral_velocity;
+    ranger_state.motion_state.steering_angle =
+        -state.motion_state.steering_angle * 10 / 180.0 * 3.14;
+  } else {
+    ranger_state.motion_state.linear_velocity =
+        state.motion_state.linear_velocity;
+    ranger_state.motion_state.angular_velocity =
+        -state.motion_state.angular_velocity;
+    ranger_state.motion_state.lateral_velocity =
+        state.motion_state.lateral_velocity;
+    ranger_state.motion_state.steering_angle =
+        -state.motion_state.steering_angle * 10 / 180.0 * 3.14;
+  }
+
   return ranger_state;
 }
 
